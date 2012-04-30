f85ef88c0d4a3ff3e7ce2411dd9edd46885baab8
Josh Bohde <josh@joshbohde.com>
2012-04-29 19:44:05 -0500

    Add some tests.

--- test/statsd/test/core.clj
+++ test/statsd/test/core.clj
@@ -2,5 +2,21 @@
   (:use [statsd.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest encode-incr
+  (is "foo:1|c|@0.5"
+      (encode-message "foo" "c" {:value 1, :rate 0.5})))
+
+(deftest should-send-nil
+  (is true (should-send nil)))
+
+(deftest ignore-negative-rates
+  (is (nil? (normalize-rate -1))))
+
+(deftest ignore-large-rates
+  (is (nil? (normalize-rate 2))))
+
+(deftest ignore-nil-rates
+  (is (nil? (normalize-rate nil))))
+
+
+
